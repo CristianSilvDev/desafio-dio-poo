8c8ed29dd2634ccd3bf927414ab746e1717b2d2e
Cristian Silva <cristiansilvazzzz1@gmail.com>
2022-11-18 14:06:40 -0300

    feat: Herança e polimorfismo PART 2

---
+++ src/br/com/desafio/dominio/Bootcamp.java
@@ -0,0 +1,12 @@
+package br.com.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class Bootcamp {
+	private String nome;
+	private String descricao;
+	private final LocalDate dataInicial = LocalDate.now();
+	private final LocalDate dataFinal = dataInicial.plusDays(45);
+	private Set<Dev> devsInscritos = HashSet<>();
+}
